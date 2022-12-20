791639979290ab5e03dc57637abb8977dc0badc2
biti2008 <tesfaybrkti00@gmail.com>
2022-12-20 05:16:03 -0800

    My first frontend project commit

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+var task, var task2;
+export default function taskBlock(trueOrFalse) {
+   task = false;
+   task2 = true;
+
+  if (trueOrFalse) {
+     task = true;
+     task2 = false;
+  }
+
+  return [task, task2];
+}
